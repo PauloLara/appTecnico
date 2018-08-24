f847fe54fd79711b347ab4a698781cecea73fad8
PauloLara <pauloalara@gmail.com>
2018-08-23 21:39:01 -0300

    Carrousel

---
+++ app/src/main/java/com/example/paulo/apptecnico/CarrosselAdaptor.java
@@ -0,0 +1,49 @@
+package com.example.paulo.apptecnico;
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class CarrosselAdaptor extends PagerAdapter {
+
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private Integer [] carousel = {R.drawable.guia1, R.drawable.guia2, R.drawable.guia3, R.drawable.guia4};
+
+    public CarrosselAdaptor(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return carousel.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.custom_layout,null);
+        ImageView imageView = (ImageView) view.findViewById(R.id.imageView);
+        imageView.setImageResource(carousel[position]);
+
+        ViewPager vp = (ViewPager) container;
+        vp.addView(view, 0);
+        return view;
+    }
+
+    @Override
+    public void destroyItem( ViewGroup container, int position, Object object) {
+        ViewPager vp = (ViewPager) container;
+        View view = (View) object;
+        vp.removeView(view);
+    }
+}
--- app/src/main/java/com/example/paulo/apptecnico/Inicial.java
+++ app/src/main/java/com/example/paulo/apptecnico/Inicial.java
@@ -1,16 +1,26 @@
 package com.example.paulo.apptecnico;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class Inicial extends AppCompatActivity {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial);
+
+        viewPager = (ViewPager) findViewById(R.id.carrossel);
+
+        CarrosselAdaptor carrosselAdaptor = new CarrosselAdaptor(this);
+
+        viewPager.setAdapter(carrosselAdaptor);
+
     }
 
     public void clickBtnIrPartidaConfig(View view){
